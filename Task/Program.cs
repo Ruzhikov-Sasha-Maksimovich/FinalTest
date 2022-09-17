8abbb236ee393db517e3fdcf680a380afea283f4
Sasha <sasaruznikovo@gmail.com>
2022-09-17 14:02:13 +0800

    Добавил рандом для того чтобы сделать ветвления в коде

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,3 +1,7 @@
 ﻿string[] array1 = new string [4] {"Hello", "2", "world", ":-)"};
 string[] array2 = new string [4] {"1234", "1567", "-2", "computer science"};
 string[] array3 = new string [4] {"Russia", "Denmark", "Kazan", "Sibir"};
+
+int random1 = new Random().Next(5);
+int random2 = new Random().Next(5);
+int random3 = new Random().Next(5);
