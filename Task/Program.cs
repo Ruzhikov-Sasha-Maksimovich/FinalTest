71fc9d9af580d0b70fc455d213c80a47a9aaf4a5
Sasha <sasaruznikovo@gmail.com>
2022-09-17 14:44:34 +0800

    Добавил влетвление но пока есть ошибки

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,6 +2,16 @@
 string[] array2 = new string [4] {"1234", "1567", "-2", "computer science"};
 string[] array3 = new string [4] {"Russia", "Denmark", "Kazan", "Sibir"};
 
+
 int random1 = new Random().Next(5);
 int random2 = new Random().Next(5);
 int random3 = new Random().Next(5);
+int rendomFile = (random);
+
+if (rendomFile < 3)
+{
+    System.Console.Write(array1[random]);
+    System.Console.Write(", ");
+    System.Console.Write(array2[random]);
+    System.Console.Write(", ");
+}
