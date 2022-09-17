e59dcd009865035cd8aff78de56f854c6db0f7d6
Sasha <sasaruznikovo@gmail.com>
2022-09-17 16:24:59 +0800

    окончательно сделал задачу

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,24 +1,23 @@
 ﻿string[] array1 = new string [4] {"Hello", "2", "world", ":-)"};
 string[] array2 = new string [4] {"1234", "1567", "-2", "computer science"};
 string[] array3 = new string [4] {"Russia", "Denmark", "Kazan", "Sibir"};
+int random1 = new Random().Next(5);
+int random2 = new Random().Next(5);
+int random3 = new Random().Next(5);
 
 int random = new Random().Next(2 ,4);
 int rendomFile = (random);
 if (rendomFile < 3)
 {
-    System.Console.Write(array1[random]);
+    System.Console.Write(array1[random1]);
     System.Console.Write(", ");
-    System.Console.Write(array2[random]);
-    System.Console.Write(", ");
+    System.Console.Write(array2[random2]);
 }
 else
 {
-    System.Console.Write(array1[random]);
+    System.Console.Write(array1[random1]);
     System.Console.Write(", ");
-    System.Console.Write(array2[random]);
+    System.Console.Write(array2[random2]);
     System.Console.Write(", ");
-    System.Console.Write(array3[random]);    
+    System.Console.Write(array3[random3]);    
 }
-int random1 = new Random().Next(5);
-int random2 = new Random().Next(5);
-int random3 = new Random().Next(5);
