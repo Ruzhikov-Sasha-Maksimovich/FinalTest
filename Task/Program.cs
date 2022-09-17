e1f8ef0c307e47f357305cde79ee8e8e7d169994
Sasha <sasaruznikovo@gmail.com>
2022-09-17 13:57:05 +0800

    Добавил переменные

---
+++ Task/Program.cs
@@ -0,0 +1,3 @@
+﻿string[] array1 = new string [4] {"Hello", "2", "world", ":-)"};
+string[] array2 = new string [4] {"1234", "1567", "-2", "computer science"};
+string[] array3 = new string [4] {"Russia", "Denmark", "Kazan", "Sibir"};
