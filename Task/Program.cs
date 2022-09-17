c7db3c64e887e5f71ac25b9bb651718c2ff988fd
Sasha <sasaruznikovo@gmail.com>
2022-09-17 15:55:56 +0800

    Добавил влетвление ещё одно влетвление и убрал ошибки

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,12 +2,8 @@
 string[] array2 = new string [4] {"1234", "1567", "-2", "computer science"};
 string[] array3 = new string [4] {"Russia", "Denmark", "Kazan", "Sibir"};
 
-
-int random1 = new Random().Next(5);
-int random2 = new Random().Next(5);
-int random3 = new Random().Next(5);
+int random = new Random().Next(2 ,4);
 int rendomFile = (random);
-
 if (rendomFile < 3)
 {
     System.Console.Write(array1[random]);
@@ -15,3 +11,14 @@ if (rendomFile < 3)
     System.Console.Write(array2[random]);
     System.Console.Write(", ");
 }
+else
+{
+    System.Console.Write(array1[random]);
+    System.Console.Write(", ");
+    System.Console.Write(array2[random]);
+    System.Console.Write(", ");
+    System.Console.Write(array3[random]);    
+}
+int random1 = new Random().Next(5);
+int random2 = new Random().Next(5);
+int random3 = new Random().Next(5);
